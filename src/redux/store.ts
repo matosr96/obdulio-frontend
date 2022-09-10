c43bc526d876b325a9c78f1c507ddf464ec40930
Edgar Matos <94972642+matosr96@users.noreply.github.com>
2022-09-10 15:47:26 -0500

    update

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {
+  applyMiddleware,
+  combineReducers,
+  compose,
+  createStore,
+  Dispatch,
+} from "redux";
+import thunk from "redux-thunk";
+import * as RR from "react-redux";
+import { LoginReducer } from "@/reducers";
 
-export interface AppStore {
+const initialState = {
+  userSingin: {
+    userInfo: localStorage.getItem("userInfo")
+      ? JSON.parse(localStorage.getItem("userInfo") || "")
+      : null,
+  },
+};
+
+const reducer = combineReducers({
+  userSingin: LoginReducer,
+});
+
+const composeEnhancer = compose;
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
+
+type StoreEvent = any;
+interface Store {
+  reviews: string;
 }
-
-export default configureStore<AppStore>({
-    reducer: {
-
-    }
-}) 
+export const useSelector: RR.TypedUseSelectorHook<Store> = RR.useSelector;
+export const useDispatch = () => RR.useDispatch<Dispatch<StoreEvent>>();
+export default store;
