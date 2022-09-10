65e46c93ff24968fba31a397d022d143d0ab0f80
Edgar Matos <94972642+matosr96@users.noreply.github.com>
2022-09-10 12:19:24 -0500

    Initial commit

---
+++ src/Application.tsx
@@ -0,0 +1,20 @@
+import { Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoadingBox from "./components/Layout/LoadingBox";
+import NotFound from "./screens/NotFount";
+
+const Application = () => {
+
+  return (
+    <Suspense fallback={<LoadingBox />}>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoadingBox />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      </BrowserRouter>
+    </Suspense>
+  )
+}
+
+export default Application;
---
+++ src/components/Layout/LoadingBox.tsx
@@ -0,0 +1,12 @@
+import '../../styles/loading.css'
+
+export const LoadingBox = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+        <p>Cargando...</p>
+    </div>
+  )
+}
+
+export default LoadingBox
