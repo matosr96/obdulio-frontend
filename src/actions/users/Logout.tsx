9629f391d7437c0fdd62b6b5a0c7e1ef8c2d78b7
Edgar Matos <94972642+matosr96@users.noreply.github.com>
2022-09-11 21:33:09 -0500

    update

--- src/actions/users/Logout.tsx
+++ src/actions/users/Logout.tsx
@@ -3,5 +3,5 @@ import { USER_SIGNOUT } from "../../constants";
 export const signout = () => (dispatch: any) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_SIGNOUT });
-  window.location.href = "/signin";
+  window.location.href = "/login";
 };
