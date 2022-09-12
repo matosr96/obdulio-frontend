8dfed6ef1928b8b2067a0e9341922ba0c5c198e5
Edgar Matos <94972642+matosr96@users.noreply.github.com>
2022-09-11 21:34:51 -0500

    login

--- src/actions/users/Logout.tsx
+++ src/actions/users/Logout.tsx
@@ -1,7 +1,8 @@
+import { Navigate } from "react-router-dom";
 import { USER_SIGNOUT } from "../../constants";
 
 export const signout = () => (dispatch: any) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_SIGNOUT });
-  window.location.href = "/login";
+  <Navigate to="/login"/>
 };
